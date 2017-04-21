09a796832194a3108aeb34984f489c3ff9e346bc
thitiwat-buatip <Subin@Thitiwats-Mac.local>
2017-04-21 20:29:42 +0700

    SocialNetwork

---
+++ SocialNetwork/SingInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SocialNetwork
+//
+//  Created by Thitiwat on 4/21/2560 BE.
+//  Copyright © 2560 Thitiwat. All rights reserved.
+//
+
+import UIKit
+
+class SingInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
