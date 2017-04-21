1be81007631a2d42bd5ac204824cf5c34d95dbcb
thitiwat-buatip <Subin@Thitiwats-Mac.local>
2017-04-21 22:51:42 +0700

    face Auth

--- SocialNetwork/SingInVC.swift
+++ SocialNetwork/SingInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SingInVC: UIViewController {
 
@@ -20,6 +23,37 @@ class SingInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookTapped(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("Error : \(String(describing: error))")
+            }
+            else if result?.isCancelled == true {
+                print("User canceled")
+            }
+            
+            else {
+                print("Successfully")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.FirebaseAuth(credential)
+            }
+        }
+    }
+    
+    func FirebaseAuth(_ credential : FIRAuthCredential){
+        
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Error : \(String(describing: error))")
+            }
+            else {
+                print("Successfully with Firebase")
+            }
+        })
+    }
 
 }
 
